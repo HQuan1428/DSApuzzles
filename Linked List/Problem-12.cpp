e1b4be3bf2ab9083b6ca4231ac8e95e06c82d1a5
H Quan <quannguyenhai51@gmail.com>
2023-10-02 13:04:41 +0700

    clone the list

--- Linked List/Problem-12.cpp
+++ Linked List/Problem-12.cpp
@@ -1,7 +1,14 @@
 /* Problem: Cloning the list 
 Solution: 
 1. HashTable
-
+    Time complexity: O(n), Space complexity: O(n).
+2. optimus space complexity. 
+    Step1:  put temp->random in temp2
+            So that we can reuse the temp->random field to point to temp2.
+    Step2:  setting temp2->random. temp2->next is the old copy of the node that
+            temp2->random shold point to, so temp->next->random is the new copy.
+    Step3:  Repair damage to old list and fill in next pointer in new list.
+    Time complexity: O(n), Space complexity: O(1). 
 */
 
 #include<bits/stdc++.h>
@@ -14,8 +21,9 @@ class ListNode {
 private:
     struct Node {
         T data;
-        Node *next; 
-        Node(const T& val): data(val), next(nullptr) {}
+        Node *next;
+        Node *random; 
+        Node(const T& val): data(val), next(nullptr), random(nullptr) {}
     };
     Node *head;
 public:
@@ -60,11 +68,41 @@ public:
         X = head; 
         while(X) {
             Y = mp[X];
-            Y->next = mp[X->next]; 
+            Y->next = mp[X->next];
+            Y->random = mp[Y->random]; 
             X = X->next; 
         }
         return mp[head]; 
     }
+    Node* Clone2(Node *head) {
+        Node *cur, *temp;
+        //step 1 
+        cur = head; 
+        while(cur) {
+            temp = new Node(cur->data); 
+            temp->next = cur->random;
+            cur->random = temp; 
+            cur = cur->next; 
+        }
+        //step 2
+        cur = head; 
+        while(cur) {
+            temp = cur->random; 
+            temp->random = temp->next->random; 
+            cur = cur->next; 
+        }
+        Node *newHead = head->random; 
+        //step 3. 
+        cur = head; 
+        while(cur) {
+            temp = cur->random;
+            cur->random = temp->next;  
+            temp->next = cur->next->random; 
+            cur = cur->next; 
+        }
+        return newHead;
+    }
+
 };
 
 
@@ -75,7 +113,7 @@ int main(int argc, char const *argv[]){
         sll.Insert(i); 
     }
     sll.print(sll.getHead());
-    sll.print(sll.clone(sll.getHead()));
+    sll.print(sll.Clone2(sll.getHead()));
     system("pause");
     return 0;
 }
