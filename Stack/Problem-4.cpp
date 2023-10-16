6dd82e5f872b86ab47faa8d848370a63f566684e
H Quan <quannguyenhai51@gmail.com>
2023-10-16 10:01:09 +0700

    Finding Spans

--- Stack/Problem-4.cpp
+++ Stack/Problem-4.cpp
@@ -1,5 +1,10 @@
 /*
-Problem-22: Finding spans  
+Problem-22: Finding spans
+    Time complexity: O(n^2), Space complexity: O(1)
+Problem-23: Finding spans (Improve the complexity).
+Solution: Finding the closest the day preceding i, such that the price is greater on that day than the price on the day i
+    Time complexity: O(n), Space complexity: O(n) [for stack]
+
 */
 #include <iostream>
 #include <string>
@@ -26,6 +31,25 @@ int* FindingSpans(int n, int *a) {
             j = j + 1; 
         }
         s[i] = j; 
+    }
+    return s; 
+}
+//iprove complexity
+int* FindingSpansImprove(int n, int *a) {
+    int *s = new int[n]; 
+    std::stack<int> st; 
+    int p = 0; //contains the closest day preceding i 
+    for(int i = 0; i < n; i++) {
+        while(!st.empty() && a[i] > a[st.top()]) {
+            st.pop(); 
+        }
+        if(st.empty()) {
+            p = -1;
+        } else {
+            p = st.top(); 
+        }
+        s[i] = i - p; 
+        st.push(i);
     }
     return s; 
 }
